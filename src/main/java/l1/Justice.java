e361497cca7de0c788fbf6959cbd8c863a596cc6
Bogdan <montibapoom@gmail.com>
2019-09-21 14:12:28 +0300

    /

--- src/main/java/l1/Justice.java
+++ src/main/java/l1/Justice.java
@@ -11,8 +11,11 @@ public class Justice {
         int price= sc.nextInt();
         System.out.println("how much corn is needed");
         int count = sc.nextInt();
-        if ((count*price)<money) System.out.println("Bon Appetit! surrender "+(money-count*price));
-        else System.out.println("you do not have enough money");
+        if(money<0 || price<0 || count<0) System.exit(0);
+        else{
+            if ((count*price)<money) System.out.println("Bon Appetit! surrender "+(money-count*price));
+            else System.out.println("you do not have enough money");
+        }
 
     }
 }
