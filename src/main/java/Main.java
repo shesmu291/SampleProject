76579e2ee3bda8a197f1d860b3b7005b82bf6e0a
Bogdan <montibapoom@gmail.com>
2019-09-21 13:30:59 +0300

    all type

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,15 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Alloha world");
+        byte by=4; //1b
+        short sh=678;//2b
+        int in=0;//4b
+        long lo=7654909;//8b
+        float fl=5.6f;//4b
+        double db=654.8;//8b
+        char c='X';//2b
+        boolean b=false;//1b
+        System.out.println(by);
+
     }
 }
