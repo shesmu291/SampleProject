d499435dcd17b1cb3949373e4d2c891bfd838a1c
Bogdan <montibapoom@gmail.com>
2019-09-22 11:34:48 +0300

    power 2

---
+++ src/main/java/l1/Degree2.java
@@ -0,0 +1,32 @@
+package l1;
+
+import java.util.Scanner;
+
+
+public class Degree2 {
+    public static boolean isPowerOfTwo(int nu) {
+        //Scanner sc=new Scanner(System.in);
+        //System.out.println("insert the number\n");
+
+        while (nu != 1 && nu % 2 == 0) {
+            nu = nu / 2;
+        }
+        if (nu == 1) {
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("insert the number\n");
+        int nu = sc.nextInt();
+        System.out.println(isPowerOfTwo(nu));
+
+    }
+
+
+
+
+}
