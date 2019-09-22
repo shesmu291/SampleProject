ed0112c11fc992b781b9842fc87b57d5e1633d99
Bogdan <montibapoom@gmail.com>
2019-09-22 10:34:11 +0300

    min

--- src/main/java/l1/Min.java
+++ src/main/java/l1/Min.java
@@ -9,9 +9,9 @@ public class Min {
         int n1 = sc.nextInt();
         int n2 = sc.nextInt();
         int n3 = sc.nextInt();
-        if (n1 < n2 && n1 < n3) System.out.println("min = " + n1);
+        if (n1 <= n2 && n1 <= n3) System.out.println("min = " + n1);
         else {
-            if (n2 < n1 && n2 < n3) System.out.println("min = " + n2);
+            if (n2 <= n1 && n2 <= n3) System.out.println("min = " + n2);
             else System.out.println("min = " + n3);
         }
     }
