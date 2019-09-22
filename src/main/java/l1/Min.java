66b22cd19eb2dc2ea533496760db4a207d36ef57
Bogdan <montibapoom@gmail.com>
2019-09-22 10:27:51 +0300

    min

---
+++ src/main/java/l1/Min.java
@@ -0,0 +1,18 @@
+package l1;
+
+import java.util.Scanner;
+
+public class Min {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter 3 numbers");
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int n3 = sc.nextInt();
+        if (n1 < n2 && n1 < n3) System.out.println("min = " + n1);
+        else {
+            if (n2 < n1 && n2 < n3) System.out.println("min = " + n2);
+            else System.out.println("min = " + n3);
+        }
+    }
+}
