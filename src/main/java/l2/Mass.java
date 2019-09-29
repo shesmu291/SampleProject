78d9c07a56caf648ed97c5f9f9c172da8f9c63de
Bogdan <montibapoom@gmail.com>
2019-09-29 12:13:25 +0300

    test and array sred ar

--- src/main/java/l2/Mass.java
+++ src/main/java/l2/Mass.java
@@ -6,17 +6,14 @@ public class Mass {
     public static int sredArr(int[] arr){
         int count=0;
         int summ=0;
+        if(arr.length == 0){
+            return 0;
+        }
         for(int i=0; i < arr.length; i++){
             count++;
-            summ=summ+arr[i];
+            summ = summ + arr[i];
         }
-        return summ/count;
+        return Math.abs(summ/count);
     }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] mas=new int[n];
 
-
-    }
 }
--- src/test/java/TestMass.java
+++ src/test/java/TestMass.java
@@ -15,4 +15,22 @@ public class TestMass {
         int result = Mass.sredArr(arr);
         assertEquals(0, result);
     }
+    @Test
+    public void testNegPos() {
+        int[] arr = {-1, -3, 4, 12};
+        int result = Mass.sredArr(arr);
+        assertEquals(3, result);
+    }
+    @Test
+    public void testNeg() {
+        int[] arr = {-1, -3, -4, -12};
+        int result = Mass.sredArr(arr);
+        assertEquals(5, result);
+    }
+    @Test
+    public void testNo() {
+        int[] arr = {};
+        int result = Mass.sredArr(arr);
+        assertEquals(0, result);
+    }
 }
