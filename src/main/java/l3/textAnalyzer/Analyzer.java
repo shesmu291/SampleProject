2b144c8c80605b9377e706669c4253dec39ff1d6
Bogdan <montibapoom@gmail.com>
2019-10-05 14:26:08 +0300

    анализатор(посмотреть как написали дргуие, переписать у них спам)

--- src/main/java/l3/textAnalyzer/Analyzer.java
+++ src/main/java/l3/textAnalyzer/Analyzer.java
@@ -1,7 +1,12 @@
 package l3.textAnalyzer;
 
 public class Analyzer {
+    public static void Bol(String text){
+
+
+    }
     public static void main(String[] args){
+
         /*LengthAnalyzer lengthAnalyzer = new LengthAnalyzer(300);
         //SpamAnalyzer spamAnalyzer = new s
         SwerAnalyzer swerAnalyzer = new SwerAnalyzer();
@@ -10,6 +15,9 @@ public class Analyzer {
         analyses[0] = new LengthAnalyzer(200);
         //analyses[1]=Spam
         analyses[1]=new SwerAnalyzer();
-        String text
+        String text="I fuck you";
+        for(int i=0; i<analyses.length; i++){
+            System.out.println(analyses[i].analyse(text));
+        }
     }
 }
